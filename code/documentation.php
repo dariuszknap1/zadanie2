86bb751e8b172d48367037f1e77ebb6ef86af9de
dariuszknap1 <103114654+dariuszknap1@users.noreply.github.com>
2022-06-11 23:20:54 +0200

    Add documentation

--- code/documentation.php
+++ code/documentation.php
@@ -1,5 +1,10 @@
 <?php
 include_once('classes/Page.php');
 function display_doc(){
-    echo "Dokumentacja";
+    ?>
+    Aplikacja służąca do obliczania wartości ciągu geometrycznego.<br>
+    Serwer Nginx + interpreter php + baza danych MySQL.<br>
+    Pomimo prób nie udało mi się połączyć z bazą danych MySQL, a więc dane są przechowywane na serwerze w sesji
+    <img src="arch.png">
+    <?php
 }
